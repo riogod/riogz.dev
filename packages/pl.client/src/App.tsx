bc09c908876b9256bf6c3c76eb2ebb6af47954f1
riogod <riogod@gmail.com>
2023-08-14 19:36:31 +0300

    add first app

---
+++ packages/pl.client/src/App.tsx
@@ -0,0 +1,25 @@
+
+import dog from './assets/dog.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a target="_blank">
+          <img src={dog} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="card">
+
+      </div>
+      <div className="read-the-docs">
+          We working on it ...
+      </div>
+    </>
+  )
+}
+
+export default App
