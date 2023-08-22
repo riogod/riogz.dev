0806d2c7df33555a5711bb4ff6c5ca3b4dc4c930
riogod <riogod@gmail.com>
2023-08-22 13:36:06 +0300

    fix admin

--- packages/pl.admin/src/modules/auth/ui/Login/index.tsx
+++ packages/pl.admin/src/modules/auth/ui/Login/index.tsx
@@ -45,7 +45,7 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Логинама!
+            Логинама!!!!
           </Typography>
           <Box
             component="form"
